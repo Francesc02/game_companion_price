65925c400492a8cadca2c6d6e6aae4b0317d2336
Francesc02 <125460353+Francesc02@users.noreply.github.com>
2026-08-19 15:09:43 +0200

    Register new routed frontend pages

--- primeng-quickstart-cli-master/src/app/app.module.ts
+++ primeng-quickstart-cli-master/src/app/app.module.ts
@@ -9,22 +9,13 @@ import { ContattiComponent } from './contatti/contatti.component';
 import { GeneriComponent } from './generi/generi.component';
 import { SharedModule } from './shared/shared.module';
 import { RootComponent } from './root/root.component';
+import { OfferteComponent } from './pages/offerte/offerte.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 @NgModule({
-  declarations: [
-    RootComponent,
-    HeaderComponent,
-    HomePageComponent,
-    DettaglioComponent,
-    ContattiComponent,
-    GeneriComponent
-  ],
-  imports: [
-    SharedModule,
-    MatCardModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  declarations: [RootComponent,HeaderComponent,HomePageComponent,DettaglioComponent,ContattiComponent,GeneriComponent,OfferteComponent,WishlistComponent,AuthComponent],
+  imports: [SharedModule,MatCardModule,FormsModule,AppRoutingModule],
   bootstrap: [RootComponent]
 })
 export class AppModule {}
