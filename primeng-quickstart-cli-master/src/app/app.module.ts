d35ab695052a5acd971d011a213b046ba1bbcb01
Francesc02 <125460353+Francesc02@users.noreply.github.com>
2026-08-19 13:20:31 +0200

    Temporarily bootstrap frontend visual preview

--- primeng-quickstart-cli-master/src/app/app.module.ts
+++ primeng-quickstart-cli-master/src/app/app.module.ts
@@ -1,45 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { DialogModule } from 'primeng/dialog';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { RatingModule } from 'primeng/rating';
-import { ToolbarModule } from 'primeng/toolbar';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { CardModule } from 'primeng/card';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MenubarModule } from 'primeng/menubar';
-import { RouterModule } from '@angular/router';
-import { ProductService } from './services/product.service';
 import { HomePageComponent } from './home-page/container/home-page.component';
 import { AppRoutingModule } from './app.routing.module';
-import { DividerModule } from 'primeng/divider';
-import { SharedModule } from './shared/shared.module';
-import { MatCardModule } from '@angular/material/card';
+import { ProductService } from './services/product.service';
 import { DettaglioComponent } from './dettaglio/dettaglio.component';
 import { ContattiComponent } from './contatti/contatti.component';
 import { GeneriComponent } from './generi/generi.component';
+import { SharedModule } from './shared/shared.module';
+import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomePageComponent,DettaglioComponent,ContattiComponent,GeneriComponent],
-  imports: [SharedModule, MatCardModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    DettaglioComponent,
+    ContattiComponent,
+    GeneriComponent,
+    PreviewComponent
+  ],
+  imports: [
+    SharedModule,
+    MatCardModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
-    ConfirmationService,
     ConfirmationService,
     MessageService,
     ProductService,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [PreviewComponent],
 })
 export class AppModule {}
