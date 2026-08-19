dcfd4b6ff4a134e1a506217ca5723602deeaec1f
Francesc02 <125460353+Francesc02@users.noreply.github.com>
2026-08-19 13:44:42 +0200

    Clean Angular routes and wildcard handling

--- primeng-quickstart-cli-master/src/app/app.routing.module.ts
+++ primeng-quickstart-cli-master/src/app/app.routing.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/container/home-page.component';
-import { AppComponent } from './app.component';
 import { DettaglioComponent } from './dettaglio/dettaglio.component';
-import { HeaderComponent } from './header/header.component';
 import { ContattiComponent } from './contatti/contatti.component';
 import { GeneriComponent } from './generi/generi.component';
 
 const routes: Routes = [
+  { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'home/:query', component: HomePageComponent },
-  { path: 'contatti', component:ContattiComponent},
-  { path: 'generi/:query', component:GeneriComponent},
-  { path: 'dettaglio/:id', component:DettaglioComponent},
-  { path: '**', component:HomePageComponent},
-  { path: '', component:HomePageComponent},
-  
+  { path: 'contatti', component: ContattiComponent },
+  { path: 'generi/:query', component: GeneriComponent },
+  { path: 'dettaglio/:id', component: DettaglioComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
