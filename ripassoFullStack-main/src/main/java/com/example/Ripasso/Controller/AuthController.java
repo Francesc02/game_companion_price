b8e760fe29679b9c1ae47422e1816ef7f3a78b9a
Francesc02 <125460353+Francesc02@users.noreply.github.com>
2026-08-19 15:34:20 +0200

    Add auth controller

---
+++ ripassoFullStack-main/src/main/java/com/example/Ripasso/Controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.Ripasso.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.Ripasso.DTO.AuthRequest;
+import com.example.Ripasso.DTO.AuthResponse;
+import com.example.Ripasso.Service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
