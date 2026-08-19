3c90370c095f1c4466136c1f6e400e3014657e8c
Francesc02 <125460353+Francesc02@users.noreply.github.com>
2026-08-19 13:44:50 +0200

    Clean SharedModule providers and formatting

--- primeng-quickstart-cli-master/src/app/shared/shared.module.ts
+++ primeng-quickstart-cli-master/src/app/shared/shared.module.ts
@@ -1,26 +1,27 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RouterModule } from "@angular/router";
-import { ConfirmationService, MessageService } from "primeng/api";
-import { ButtonModule } from "primeng/button";
-import { CardModule } from "primeng/card";
-import { ConfirmDialogModule } from "primeng/confirmdialog";
-import { DialogModule } from "primeng/dialog";
-import { DividerModule } from "primeng/divider";
-import { DropdownModule } from "primeng/dropdown";
-import { InputNumberModule } from "primeng/inputnumber";
-import { InputTextModule } from "primeng/inputtext";
-import { InputTextareaModule } from "primeng/inputtextarea";
-import { MenubarModule } from "primeng/menubar";
-import { RadioButtonModule } from "primeng/radiobutton";
-import { RatingModule } from "primeng/rating";
-import { TableModule } from "primeng/table";
-import { ToolbarModule } from "primeng/toolbar";
-import { ProductService } from "../services/product.service";
-import { MatCardModule } from "@angular/material/card";
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MenubarModule } from 'primeng/menubar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ProductService } from '../services/product.service';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -42,7 +43,7 @@ import { MatCardModule } from "@angular/material/card";
     RouterModule,
     CardModule,
     DividerModule,
-    MatCardModule,
+    MatCardModule
   ],
   exports: [
     BrowserModule,
@@ -63,14 +64,9 @@ import { MatCardModule } from "@angular/material/card";
     MenubarModule,
     RouterModule,
     CardModule,
-    DividerModule,
+    DividerModule
   ],
-  providers: [
-    ConfirmationService,
-    ConfirmationService,
-    MessageService,
-    ProductService,
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [ConfirmationService, MessageService, ProductService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule {}
